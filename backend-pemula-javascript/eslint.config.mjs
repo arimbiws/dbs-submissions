369bdeca086ae9d8ab7c99f1d279e66878b2c348
arimbi wirasetia <arimbiwirasetia@gmail.com>
2026-03-26 14:03:21 +0800

    Bookshelf API

---
+++ backend-pemula-javascript/eslint.config.mjs
@@ -0,0 +1,16 @@
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import daStyle from 'eslint-config-dicodingacademy';
+
+/** @type { import('eslint').Linter.Config[] } */
+export default [
+  daStyle,
+  {
+    languageOptions: {
+      globals: {
+        ...globals.node,
+      },
+    },
+  },
+  pluginJs.configs.recommended,
+];
